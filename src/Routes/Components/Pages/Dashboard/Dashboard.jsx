4d41b69eeb7e64793f6a7b7f604c535d9d345a9f
nahin440 <rafironin@gmail.com>
2025-02-24 22:20:43 +0600

    reminder

--- src/Routes/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Routes/Components/Pages/Dashboard/Dashboard.jsx
@@ -4,8 +4,8 @@ import { Link, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     return (
-        <div className="grid grid-cols-12 ">
-            <section className="col-span-2 py-10" >
+        <div className="grid grid-cols-12  ">
+            <section className="col-span-2 grid-flow-col-dense " >
                 <div className="drawer lg:drawer-open">
                     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                     <div className="drawer-content flex flex-col items-center justify-center">
@@ -15,8 +15,8 @@ const Dashboard = () => {
                         </label>
                     </div>
                     <div className="drawer-side  ">
-                        <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                        <label htmlFor="my-drawer-2" aria-label="close sidebar" className=" "></label>
+                        <ul className="menu bg-base-200 text-base-content min-h-full w-56 p-4">
                             {/* Sidebar content here */}
                             <li><Link to="my-tasks">My Tasks</Link></li>
                             <li><Link to='add-tasks'>Add Task</Link></li>
