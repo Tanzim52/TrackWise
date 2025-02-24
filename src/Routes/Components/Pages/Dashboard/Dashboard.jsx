9f005913121d8f789b9dc14c074e651d5e468af1
Tanzim52 <161871173+Tanzim52@users.noreply.github.com>
2025-02-24 22:36:43 +0600

    nothing

--- src/Routes/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Routes/Components/Pages/Dashboard/Dashboard.jsx
@@ -16,7 +16,7 @@ const Dashboard = () => {
                     </div>
                     <div className="drawer-side  ">
                         <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                        <ul className="menu bg-base-200 text-base-content min-h-full w-56 p-4">
                             {/* Sidebar content here */}
                             <li><Link to="my-tasks">My Tasks</Link></li>
                             <li><Link to='add-tasks'>Add Task</Link></li>
