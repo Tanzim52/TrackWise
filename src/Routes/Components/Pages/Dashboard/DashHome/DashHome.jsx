113b129c1b03b11845afec51a27a8ddac2560fe9
nahin440 <rafironin@gmail.com>
2025-02-26 03:26:39 +0600

    mm

--- src/Routes/Components/Pages/Dashboard/DashHome/DashHome.jsx
+++ src/Routes/Components/Pages/Dashboard/DashHome/DashHome.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
 import AddReview from '../DashHome/AddReview/AddReview'
+import { AuthContext } from '../../../AuthProvider/AuthProvider';
 
 const DashHome = () => {
+    const {user} = useContext(AuthContext)
+    console.log(user?.email,user?.displayName.user?.photoURL)
     return (
         <div>
             Welcome to Dashboard Home
