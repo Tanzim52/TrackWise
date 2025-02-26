6c5128ac56a3e59d2384f0afa0b3147e5125f401
Tanzim52 <161871173+Tanzim52@users.noreply.github.com>
2025-02-26 06:33:55 +0600

    finalized footer

--- src/Routes/Components/Footer/Footer.jsx
+++ src/Routes/Components/Footer/Footer.jsx
@@ -6,18 +6,19 @@ const Footer = () => {
             <footer className="bg-[#4c1a36] text-[#fefdec] py-10 px-5 md:px-20">
                 <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-8">
                     {/* Logo & Branding */}
-                    <div className="flex flex-col items-center md:items-start text-center md:text-left">
-                        <img
-                            src="https://i.ibb.co/7th4bsjY/round-fav-icon-for-website-trackwise-removebg-preview.png"
-                            alt="TrackWise"
-                            className="w-16 md:w-20 mb-2"
-                        />
-                        <h2 className="text-xl font-bold">TrackWise</h2>
-                        <p className="text-sm text-[#dfab81]">Track, Plan & Achieve with Ease</p>
-                        <p className="text-xs text-gray-300 mt-2">
-                            ZeroDegree Development <br />
-                            Reliability & Quality Assured
-                        </p>
+                    <div className="flex flex-col justify-start items-center md:items-start text-center md:text-left">
+                        <div className="flex items-center gap-2">
+                            <img
+                                src="https://i.ibb.co/7th4bsjY/round-fav-icon-for-website-trackwise-removebg-preview.png"
+                                alt="TrackWise"
+                                className="w-16 md:w-20 mb-2"
+                            />
+                            <div>
+                                <h2 className="text-xl font-bold">TrackWise</h2>
+                                <p className="text-sm text-[#dfab81]">Track, Plan & Achieve with Ease</p>
+                            </div>
+                        </div>
+                        
                     </div>
 
                     {/* Social Links */}
@@ -41,7 +42,7 @@ const Footer = () => {
 
             </footer>
             <div className="text-center text-xs bg-[#4c1a36] text-[#fefdec] pt-6 border-t pb-7">
-                &copy; {new Date().getFullYear()} TrackWise. All rights reserved.
+                &copy; {new Date().getFullYear()} ZeroDegree. All rights reserved.
             </div>
         </>
     );
